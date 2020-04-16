2fe6a154506ac1a89820e95473c6102a7a30a384
burningman2k20 <burningman2k9@gmail.com>
2020-04-16 15:09:18 -0400

    Update app-routing.module.ts

--- clean-blog-two/src/app/app-routing.module.ts
+++ clean-blog-two/src/app/app-routing.module.ts
@@ -2,7 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path : '',
+    redirectTo: '/home',
+    pathMatch:'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
